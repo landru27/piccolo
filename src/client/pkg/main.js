1631bc682c0773a29a3abb8699f10def4c5f435a
landru27 <landru729@gmail.com>
2020-12-12 23:12:32 -0500

    [feature] refactor application initialization for use of ECS
    Signed-off-by: landru27 <landru729@gmail.com>

--- src/client/pkg/main.js
+++ src/client/pkg/main.js
@@ -6,24 +6,41 @@ import * as Presentation from './utility/presentation.js';
 import { ThreeApp } from './threeApp.js';
 import { Statistics } from './statistics.js';
 
-import { Color, Vector3 } from 'three';
+import { Object3D, Vector3, Color } from 'three';
+import { AxesHelper, BoxGeometry, MeshPhongMaterial, Mesh } from 'three';
 
 import { World } from 'ecsy';
-import { HeightMap } from './components/HeightMap.js';
-import { Terrain } from './systems/Terrain.js';
+
+import { SceneObj } from './components/SceneObj.js';
+import { PlayerTag, PlayerKeyboardControls, PlayerPointerControls, PlayerCamera } from './components/Player.js';
+import { PlayerCameraMotion } from './systems/PlayerCameraMotion.js';
 
 
 ////////////////////////////////////////////////////////////////
-////////  detect our runtime environment
+////////  initialize three.js
 
-const devicePixelRatio = window.devicePixelRatio;
+const threeApp = new ThreeApp({
+    devicePixelRatio: window.devicePixelRatio,
+    appContainerDOMElement: 'appContainer',
+    appOverlayDOMElement: 'appOverlay',
+    keyboardControlsDOMElement: document,
+    pointerControlsDOMElement: document.body,
+    clearColor: 0x000000,
+    background: new Color('skyblue'),
+    fieldOfVision: 35,
+    nearClippingPlane: 0.1,
+    farClippingPlane: 1000,
+    cameraPosition: new Vector3(32, 32, 32),
+    cameraLookAt: new Vector3(0, 0, 0),
+});
 
-const container = document.querySelector('#canvas-container');
-let viewWidth = container.clientWidth;
-let viewHeight = container.clientHeight;
+window.addEventListener('resize', function() {
+    threeApp.resizeViewport();
+}, false);
 
-const infoOverlay = document.getElementById( 'info-overlay' );
-const infoDetails = document.getElementById( 'info' );
+const stats = new Statistics();
+stats.domElement.style.display = 'block';
+document.getElementById( 'world-area' ).appendChild( stats.domElement );
 
 
 ////////////////////////////////////////////////////////////////
@@ -31,229 +48,77 @@ const infoDetails = document.getElementById( 'info' );
 
 const world = new World();
 
-world.registerComponent(HeightMap);
-//world.registerComponent(Location);
-//world.registerComponent(Position);
-//world.registerComponent(Motion);
-//world.registerComponent(PlayerCamera);
+//world.registerComponent(ThreeAppRef);
+world.registerComponent(SceneObj);
+world.registerComponent(PlayerTag);
+world.registerComponent(PlayerKeyboardControls);
+world.registerComponent(PlayerPointerControls);
+world.registerComponent(PlayerCamera);
+//world.registerComponent(Propulsion);
+//world.registerComponent(Velocity);
+//world.registerComponent(AmbientLight);
+//world.registerComponent(HemisphereLight);
+//world.registerComponent(DirectionalLight);
+//world.registerComponent(SunCycle);
+//world.registerComponent(MoonCycle);
+//world.registerComponent(StarCycle);
 
-world.registerSystem(Terrain);
-//world.registerSystem(PlayerCommand);
-//world.registerSystem(PlayerMovement);
-//world.registerSystem(PlayerCamera);
-//world.registerSystem(Movement);
-
-
-////////////////////////////////////////////////////////////////
-////////  initialize three.js
-
-const threeApp = new ThreeApp({
-    devicePixelRatio: devicePixelRatio,
-    viewWidth: viewWidth,
-    viewHeight: viewHeight,
-    clearColor: 0x000000,
-    background: new Color('skyblue'),
-    fieldOfVision: 35,
-    aspectRatio: viewWidth / viewHeight,
-    nearClippingPlane: 0.1,
-    farClippingPlane: 1000,
-    cameraPosition: new Vector3(54, 1, 37),
-    cameraLookAt: new Vector3(58, 1, 38),
-    keyboardControlDOMElement: document,
-    pointerControlDOMElement: document.body,
-});
-
-threeApp.setAmbientLighting({
-    color: 0x7f7f7f,
-});
-
-threeApp.addDirectionalLighting({
-    color: 0xffffff,
-    intensity: 1,
-    position: new Vector3(1, 2, 3),
-});
-
-////////////////////////////////////////////////////////////////
-////////  connect three.js to the browser page
-
-container.append(threeApp.getRenderDOMElement());
-
-window.addEventListener( 'resize', onWindowResize, false );
-function onWindowResize() {
-    viewWidth = container.clientWidth;
-    viewHeight = container.clientHeight;
-
-    threeApp.resizeViewport();
-}
-
-infoDetails.addEventListener( 'click', function () {
-    threeApp.getPointerControls().lock();
-}, false );
-threeApp.getPointerControls().addEventListener( 'lock', function () {
-    infoDetails.style.display = 'none';
-    infoOverlay.style.display = 'none';
-} );
-threeApp.getPointerControls().addEventListener( 'unlock', function () {
-    infoOverlay.style.display = 'block';
-    infoDetails.style.display = '';
-} );
+//world.registerSystem(PlayerAction);
+world.registerSystem(PlayerCameraMotion);
+//world.registerSystem(CelestialCycle);
+//world.registerSystem(Motion);
+//world.registerSystem(Terrain);
 
 
 ////////////////////////////////////////////////////////////////
 ////////  populate the world
 
 // create something to show -- will move to ECS
+threeApp.getScene().add(new AxesHelper(8));
+threeApp.getScene().add(new Mesh(new BoxGeometry(1, 1, 1), new MeshPhongMaterial({color: 0x7f7f7f})));
 
-//const geometry = new BoxBufferGeometry(2, 2, 2);
-//const material = new MeshPhongMaterial({color: 'green'});
-//const cube = new Mesh(geometry, material);
-//threeApp.getScene().add(cube);
-
-////
-const aSmallLandscape = new HeightMap();
-threeApp.getScene().add(aSmallLandscape.mesh);
+// the player entity
+const playerObj = new Object3D();
+const player = world.createEntity();
+player.addComponent(SceneObj, {ref: playerObj});
+player.addComponent(PlayerTag);
+player.addComponent(PlayerKeyboardControls, {ref: threeApp.getKeyboardControls()});
+player.addComponent(PlayerPointerControls, {ref: threeApp.getPointerControls()});
+player.addComponent(PlayerCamera, {ref: threeApp.getCamera()});
 
 
 ////////////////////////////////////////////////////////////////
 ////////  main loop
 
 function animate() {
-    requestAnimationFrame( animate );
+    requestAnimationFrame(animate);
 
     stats.begin();
 
     const time = performance.now();
-    const delta = (time - prevTime) / 1000;
+    const delta = (time - prevTime) * 0.001;
     prevTime = time;
 
-    // command input
-    commands = threeApp.getKeyboardControls().getCommands();
-
-    //if (true) {
-    if (controls.isLocked === true) {
-        // dampen velocity so we don't meteor away
-        velocity.x -= velocity.x * 4 * delta;
-        velocity.y -= velocity.y * 4 * delta;
-        velocity.z -= velocity.z * 4 * delta;
-
-        // gravity
-        prevToggleGravity = toggleGravity;
-        toggleGravity = commands.toggleGravity;
-        if (toggleGravity && (toggleGravity != prevToggleGravity)) {
-            flagGravity = !flagGravity;
-        }
-        if (flagGravity) {
-            camera.position.y -= 9.8 * 3.0 * delta;
-        }
-
-        // boundaries
-        prevToggleXYZPositive = toggleXYZPositive;
-        toggleXYZPositive = commands.toggleXYZPositive;
-        if (toggleXYZPositive && (toggleXYZPositive != prevToggleXYZPositive)) {
-            flagXYZPositive = !flagXYZPositive;
-        }
-        if (flagXYZPositive) {
-            if (camera.position.x < 0.00001) {
-                camera.position.x = 0.00002;
-                velocity.x = 0;
-            }
-            if (camera.position.y < 1.00001) {
-                camera.position.y = 1.00002;
-                velocity.y = 0;
-            }
-            if (camera.position.z < 0.00001) {
-                camera.position.z = 0.00002;
-                velocity.z = 0;
-            }
-        }
-
-        // movement input
-        movement = threeApp.getKeyboardControls().getMovement();
-
-        direction.z = Number(movement.moveForward) - Number(movement.moveBackward);
-        direction.y = Number(movement.moveUpward) - Number(movement.moveDownward);
-        direction.x = Number(movement.moveRightward) - Number(movement.moveLeftward);
-        direction.normalize();
-
-        slew.z = Number(movement.slewForward) - Number(movement.slewBackward);
-        slew.y = Number(movement.slewUpward) - Number(movement.slewDownward);
-        slew.x = Number(movement.slewRightward) - Number(movement.slewLeftward);
-        slew.normalize();
-
-        // movement
-        velocity.z += direction.z * 40.0 * delta;
-        controls.moveForward(velocity.z * delta);
-
-        velocity.y += direction.y * 40.0 * delta;
-        controls.moveUpward(velocity.y * delta);
-
-        velocity.x += direction.x * 40.0 * delta;
-        controls.moveRightward(velocity.x * delta);
-
-        // jumping
-        prevJumping = moveJumping;
-        moveJumping = movement.moveJump;
-        if (moveJumping && (moveJumping != prevJumping)) {
-            velocity.y += 4000.0 * delta;
-            controls.moveUpward(velocity.y * delta);
-        }
-
-        // slewing
-        camera.position.x += slew.x * delta;
-        camera.position.y += slew.y * delta;
-        camera.position.z -= slew.z * delta;
-    }
+    world.execute(delta);
 
     let infoz = [
-        'camera position :  ' + Presentation.vector3To4Places(camera.position),
-        'camera velocity :  ' + Presentation.vector3To4Places(velocity),
-        'gravity is      :  ' + (flagGravity ? 'on' : 'off'),
-        'boundaries is   :  ' + (flagXYZPositive ? 'on' : 'off'),
+        'camera position :  ' + 'TODO',  // Presentation.vector3To4Places(player.getComponent(SceneObj).position),
+        'camera velocity :  ' + 'TODO',  // Presentation.vector3To4Places(player.getComponent(Velocity).vector),
+        'gravity is      :  ' + 'TODO',
+        'boundaries is   :  ' + 'TODO',
     ];
     stats.newInfo(infoz);
-
-    prevToggleStats = toggleStats;
-    toggleStats = commands.toggleStats;
-    if (toggleStats && (toggleStats != prevToggleStats)) {
-        stats.domElement.style.display = (stats.domElement.style.display === 'block' ? 'none' : 'block');
-    }
 
     threeApp.render();
 
     stats.end();
 }
 
-const controls = threeApp.getPointerControls();
-const camera = controls.getCamera();
-
-const stats = new Statistics();
-stats.domElement.style.display = 'block';
-document.getElementById( 'world-area' ).appendChild( stats.domElement );
-let toggleStats = false;
-let prevToggleStats = false;
-
-let toggleGravity = false;
-let prevToggleGravity = false;
-let flagGravity = true;
-
-let toggleXYZPositive = false;
-let prevToggleXYZPositive = false;
-let flagXYZPositive = true;
-
-const velocity = new Vector3();
-const direction = new Vector3();
-const slew = new Vector3();
-let moveJumping;
-let prevJumping;
-
-let prevTime = performance.now();
-let movement;
-let commands;
-
 
 ////////////////////////////////////////////////////////////////
 ////////  make it so
 
+let prevTime = performance.now();
 animate();
 
 
--- src/client/pkg/threeApp.js
+++ src/client/pkg/threeApp.js
@@ -4,8 +4,7 @@
 import { setDefaultIfNoValue } from './utility/comparison.js';
 
 import { Object3D, Vector3, Color } from 'three';
-import { Clock, WebGLRenderer, Scene } from 'three';
-import { AmbientLight, HemisphereLight, DirectionalLight, Fog } from 'three';
+import { Clock, Scene, WebGLRenderer } from 'three';
 import { PerspectiveCamera } from 'three';
 import { KeyUpAndDownControls } from './controls/KeyUpAndDownControls.js';
 import { PointerLockControls } from './controls/PointerLockControls.js';
@@ -15,52 +14,95 @@ import { PointerLockControls } from './controls/PointerLockControls.js';
 ////////  the top level elements of a running three.js application
 
 export class ThreeApp {
+
     constructor(options) {
+        const scope = this;
+
         // use the provided or default values
         this.devicePixelRatio = setDefaultIfNoValue(options.devicePixelRatio, 1);
-        this.viewWidth = setDefaultIfNoValue(options.viewWidth, 256);
-        this.viewHeight = setDefaultIfNoValue(options.viewHeight, 256);
+        this.appContainerDOMElement = setDefaultIfNoValue(options.appContainerDOMElement, 'appContainer');
+        this.appOverlayDOMElement = setDefaultIfNoValue(options.appOverlayDOMElement, 'appOverlay');
+        this.keyboardControlsDOMElement = setDefaultIfNoValue(options.keyboardControlsDOMElement, document);
+        this.pointerControlsDOMElement = setDefaultIfNoValue(options.pointerControlsDOMElement, document.body);
         this.clearColor = setDefaultIfNoValue(options.clearColor, 0x000000);
         this.background = setDefaultIfNoValue(options.background, 0x000000);
         this.fieldOfView = setDefaultIfNoValue(options.fieldOfView, 45);
-        this.aspectRatio = setDefaultIfNoValue(options.aspectRatio, (this.viewWidth / this.viewHeight));
         this.nearClippingPlane = setDefaultIfNoValue(options.nearClippingPlane, 0.1);
         this.farClippingPlane = setDefaultIfNoValue(options.farClippingPlane, 1000);
         this.cameraPosition = setDefaultIfNoValue(options.cameraPosition, new Vector3(3, 0, 6));
         this.cameraLookAt = setDefaultIfNoValue(options.cameraLookAt, new Vector3(0, 0, 0));
-        this.keyboardControlDOMElement = setDefaultIfNoValue(options.keyboardControlDOMElement, document);
-        this.pointerControlDOMElement = setDefaultIfNoValue(options.pointerControlDOMElement, document.body);
+
+        // viewport
+        this.appContainer = document.getElementById(this.appContainerDOMElement);
+        this.appOverlay = document.getElementById(this.appOverlayDOMElement);
+        this.viewportWidth = appContainer.clientWidth;
+        this.viewportHeight = appContainer.clientHeight;
+        this.aspectRatio = (this.viewportWidth / this.viewportHeight);
+
+        // user controls
+        this.keyboardControls = new KeyUpAndDownControls(this.keyboardControlsDOMElement);
+        this.pointerControls = new PointerLockControls(this.pointerControlsDOMElement);
 
         // timing clock
         this.clock = new Clock();
 
-        // WebGL renderer
-        this.renderer = new WebGLRenderer();
-        this.renderer.setPixelRatio(this.devicePixelRatio);
-        this.renderer.setSize(this.viewWidth, this.viewHeight);
-        this.renderer.setClearColor(this.clearColor);
-
         // threejs scene
         this.scene = new Scene();
         this.scene.background = new Color(this.background);
-        this.ambientLight = null;
-        this.hemisphereLight = null;
-        this.directionalLights = [];
-        this.distanceFog = null;
 
         // user camera
         this.camera = new PerspectiveCamera(this.fieldOfView, this.aspectRatio, this.nearClippingPlane, this.farClippingPlane);
         this.camera.position.copy(this.cameraPosition);
         this.camera.lookAt(this.cameraLookAt);
+        this.scene.add(this.camera);
 
-        // user controls
-        this.keyboardControls = new KeyUpAndDownControls(this.keyboardControlDOMElement);
-        this.pointerControls = new PointerLockControls(this.camera, this.pointerControlDOMElement);
-        this.scene.add(this.pointerControls.getCamera());
+        // WebGL renderer
+        this.renderer = new WebGLRenderer();
+        this.renderer.setPixelRatio(this.devicePixelRatio);
+        this.renderer.setSize(this.viewportWidth, this.viewportHeight);
+        this.renderer.setClearColor(this.clearColor);
+
+        // connect this three.js app to its execution environment
+        this.appContainer.append(this.renderer.domElement);
+
+        this.appOverlay.addEventListener('click', function () {
+            scope.pointerControls.lock();
+        }, false);
+
+        this.pointerControls.addEventListener('lock', function () {
+            scope.appOverlay.style.display = 'none';
+        });
+
+        this.pointerControls.addEventListener('unlock', function () {
+            scope.appOverlay.style.display = 'block';
+        });
     }
 
-    getRenderer() {
-        return this.renderer;
+    ////////////////////////////////
+
+    getAppContainer() {
+        return this.appContainer;
+    }
+
+    getAppOverlay() {
+        return this.appOverlay;
+    }
+
+    getViewportSize() {
+        return {
+            width: this.viewportWidth,
+            height: this.viewportHeight,
+        }
+    }
+
+    ////////////////////////////////
+
+    getPointerControls() {
+        return this.pointerControls;
+    }
+
+    getKeyboardControls() {
+        return this.keyboardControls;
     }
 
     getScene() {
@@ -71,64 +113,21 @@ export class ThreeApp {
         return this.camera;
     }
 
-    getPointerControls() {
-        return this.pointerControls;
+    getRenderer() {
+        return this.renderer;
     }
 
-    getKeyboardControls() {
-        return this.keyboardControls;
-    }
+    ////////////////////////////////
 
-    getRenderDOMElement() {
-        return this.renderer.domElement;
-    }
-
-    setAmbientLighting(options) {
-        let color = setDefaultIfNoValue(options.color, 0x7F7F7F);
-        let intensity = setDefaultIfNoValue(options.intensity, 1);
-
-        this.ambientLight = new AmbientLight(color, intensity);
-        this.scene.add(this.ambientLight);
-    }
-
-    setHemisphereLighting(options) {
-        let skyColor = setDefaultIfNoValue(options.skyColor, 0xbbbbbb);
-        let groundColor = setDefaultIfNoValue(options.groundColor, 0x444444);
-        let intensity = setDefaultIfNoValue(options.intensity, 1);
-
-        this.hemisphereLight = new HemisphereLight(skyColor, groundColor, intensity);
-        this.scene.add(this.hemisphereLight);
-    }
-
-    addDirectionalLighting(options) {
-        let color = setDefaultIfNoValue(options.color, 0x7F7F7F);
-        let intensity = setDefaultIfNoValue(options.intensity, 1);
-        let position = setDefaultIfNoValue(options.position, Object3D.DefaultUp);
-
-        const light = new DirectionalLight(color, intensity);
-        light.position.copy(position).normalize();
-        this.directionalLights.push(light);
-        this.scene.add(this.directionalLights[this.directionalLights.length - 1]);
-    }
-
-    setDistanceFog(options) {
-        let color = setDefaultIfNoValue(options.color, 0xcccccc);
-        let near = setDefaultIfNoValue(options.near, 300);
-        let far = setDefaultIfNoValue(options.far, 3000);
-
-        this.distanceFog = new Fog(color, near, far);
-        this.scene.add(this.distanceFog);
-    }
-
-    resizeViewport(width, height) {
-        this.viewWidth = width;
-        this.viewHeight = height;
-        this.aspectRatio = this.viewWidth / this.viewHeight;
+    resizeViewport() {
+        this.viewportWidth = appContainer.clientWidth;
+        this.viewportHeight = appContainer.clientHeight;
+        this.aspectRatio = this.viewportWidth / this.viewportHeight;
 
         this.camera.aspect = this.aspectRatio;
         this.camera.updateProjectionMatrix();
 
-        this.renderer.setSize(this.viewWidth, this.viewHeight);
+        this.renderer.setSize(this.viewportWidth, this.viewportHeight);
     }
 
     render() {

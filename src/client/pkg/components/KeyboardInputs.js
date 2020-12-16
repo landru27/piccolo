5d5526a449b4c6c3ccb005e7292056752ce5a8da
landru27 <landru729@gmail.com>
2020-12-15 22:45:44 -0500

    [feature] add player keyboard input with simple logging of inputs
    Signed-off-by: landru27 <landru729@gmail.com>

--- src/client/pkg/components/KeyboardInputs.js
+++ src/client/pkg/components/KeyboardInputs.js
@@ -10,5 +10,33 @@ import { Component, Types } from 'ecsy';
 export class KeyboardInputs extends Component {}
 
 KeyboardInputs.schema = {
-    comingsoon: { type: Types.Number },
+    slewXPos: { type: Types.Boolean },
+    slewXNeg: { type: Types.Boolean },
+    slewYPos: { type: Types.Boolean },
+    slewYNeg: { type: Types.Boolean },
+    slewZPos: { type: Types.Boolean },
+    slewZNeg: { type: Types.Boolean },
+
+    rotateXPos: { type: Types.Boolean },
+    rotateXNeg: { type: Types.Boolean },
+    rotateYPos: { type: Types.Boolean },
+    rotateYNeg: { type: Types.Boolean },
+    rotateZPos: { type: Types.Boolean },
+    rotateZNeg: { type: Types.Boolean },
+
+    accelerateAhead: { type: Types.Boolean },
+    accelerateBack: { type: Types.Boolean },
+    accelerateLeft: { type: Types.Boolean },
+    accelerateRight: { type: Types.Boolean },
+    accelerateUp: { type: Types.Boolean },
+    accelerateDown: { type: Types.Boolean },
+
+    boostAcceleration: { type: Types.Boolean },
+    decelerateAll: { type: Types.Boolean },
+
+    toggleGravity: { type: Types.Boolean },
+    toggleDebug: { type: Types.Boolean },
+    screenshot: { type: Types.Boolean },
 };
+
+////////////////////////////////////////////////////////////////
--- src/client/pkg/systems/PlayerCameraMotion.js
+++ src/client/pkg/systems/PlayerCameraMotion.js
@@ -41,6 +41,31 @@ export class PlayerCameraMotion extends System {
             console.log(pointerInput.mouseButtonClick);
             pointerInput.mouseButtonClick = '';
         }
+
+        const keyboardInput = player.getComponent(PlayerInputs).keyboardInputs;
+        if (keyboardInput.slewXPos == true) { console.log('slewXPos'); }
+        if (keyboardInput.slewXNeg == true) { console.log('slewXNeg'); }
+        if (keyboardInput.slewYPos == true) { console.log('slewYPos'); }
+        if (keyboardInput.slewYNeg == true) { console.log('slewYNeg'); }
+        if (keyboardInput.slewZPos == true) { console.log('slewZPos'); }
+        if (keyboardInput.slewZNeg == true) { console.log('slewZNeg'); }
+        if (keyboardInput.rotateXPos == true) { console.log('rotateXPos'); }
+        if (keyboardInput.rotateXNeg == true) { console.log('rotateXNeg'); }
+        if (keyboardInput.rotateYPos == true) { console.log('rotateYPos'); }
+        if (keyboardInput.rotateYNeg == true) { console.log('rotateYNeg'); }
+        if (keyboardInput.rotateZPos == true) { console.log('rotateZPos'); }
+        if (keyboardInput.rotateZNeg == true) { console.log('rotateZNeg'); }
+        if (keyboardInput.accelerateAhead == true) { console.log('accelerateAhead'); }
+        if (keyboardInput.accelerateBack == true) { console.log('accelerateBack'); }
+        if (keyboardInput.accelerateLeft == true) { console.log('accelerateLeft'); }
+        if (keyboardInput.accelerateRight == true) { console.log('accelerateRight'); }
+        if (keyboardInput.accelerateUp == true) { console.log('accelerateUp'); }
+        if (keyboardInput.accelerateDown == true) { console.log('accelerateDown'); }
+        if (keyboardInput.boostAcceleration == true) { console.log('boostAcceleration'); }
+        if (keyboardInput.decelerateAll == true) { console.log('decelerateAll'); }
+        if (keyboardInput.toggleGravity == true) { console.log('toggleGravity'); keyboardInput.toggleGravity = false; }
+        if (keyboardInput.toggleDebug == true) { console.log('toggleDebug'); keyboardInput.toggleDebug = false; }
+        if (keyboardInput.screenshot == true) { console.log('screenshot'); keyboardInput.screenshot = false; }
     }
 
     rotateFromPointerMotion(x, y, q) {
